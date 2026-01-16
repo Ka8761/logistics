a336caab4126b28b9ba8fbb984721897d401351b
ka8761 <dikachinwankwo575@gmail.com>
2026-01-16 21:40:23 +0100

    add url encoded

--- server/api/server.js
+++ server/api/server.js
@@ -13,7 +13,9 @@ dotenv.config({ path: './config/.env' })
 
 
 const app = express();
- app.use(express.json());
+ app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cors({ origin: "*" }));
 
 
