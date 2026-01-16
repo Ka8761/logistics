fe7e0fb14fb98fc3f2222553fc0fc079b396d519
ka8761 <dikachinwankwo575@gmail.com>
2026-01-16 14:13:16 +0100

    changed to export default app

--- server/api/server.js
+++ server/api/server.js
@@ -56,4 +56,4 @@ app.use((err, req, res, next) => {
 // app.listen(process.env.PORT || 5000, () => {
 //   console.log(`Server running on port ${process.env.PORT || 5000}`);
 // });
-module.exports = app;
+export default app;
