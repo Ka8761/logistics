ec67bdeaafddbc68eca7be9892c639b947e834e9
ka8761 <dikachinwankwo575@gmail.com>
2026-01-17 02:49:24 +0100

    fix cors origin

--- server/api/server.js
+++ server/api/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'https://logistics-cargoextra.vercel.app/',  // YOUR FRONTEND URL
+  origin: 'https://logistics-cargoextra.vercel.app', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
