acdcf404caa6a269967f4d3c69271ba55ba686eb
ka8761 <dikachinwankwo575@gmail.com>
2026-01-18 11:43:50 +0100

    a few more changes

--- server/api/server.js
+++ server/api/server.js
@@ -90,7 +90,7 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
